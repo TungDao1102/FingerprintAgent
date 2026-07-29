65ef4363dd79c5a5ec5ce149d3d663e68ee9b37c
Tung Dao <daotung492@gmail.com>
2026-07-29 08:40:38 +0700

    fix(01): WR-06 add StringLength(50) validation attributes to CaptureRequest

--- src/FingerprintAgent/Models/CaptureRequest.cs
+++ src/FingerprintAgent/Models/CaptureRequest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 
 namespace FingerprintAgent.Models
@@ -6,18 +7,23 @@ namespace FingerprintAgent.Models
     public class CaptureRequest
     {
         [JsonProperty("thamChieuId")]
+        [StringLength(50)]
         public string ThamChieuId { get; set; }
 
         [JsonProperty("maPhieu")]
+        [StringLength(50)]
         public string MaPhieu { get; set; }
 
         [JsonProperty("loaiPhieu")]
+        [StringLength(50)]
         public string LoaiPhieu { get; set; }
 
         [JsonProperty("vaiKyId")]
+        [StringLength(50)]
         public string VaiKyId { get; set; }
 
         [JsonProperty("nhanLucId")]
+        [StringLength(50)]
         public string NhanLucId { get; set; }
 
         [JsonProperty("metadata")]
